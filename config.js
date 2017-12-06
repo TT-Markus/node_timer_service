0e020cd468eca49d7467e8b28ed2451c71909a4e
TT-Markus <TT-Markus@users.noreply.github.com>
2017-12-06 10:35:37 -0800

    Config change for Server 59 release

--- config.js
+++ config.js
@@ -2,10 +2,12 @@ var config = {};
 
 config.logFile = 'trace.log';
 config.logLevel = 4;
-config.redis_server_port = 6379;
+config.redis_server_port = 6380;
 config.redis_server_name = '127.0.0.1';
-config.max_retry = 5;
+config.max_retry = 10;
 config.default_retry_interval = 10; // in 10 seconds
 config.att_convert_retry_interval = 60; // in 60 seconds for transmog
-config.no_retry_services = ['value'];
+config.forever_retry_services = ["tt_archive"];
+config.max_retry_interval = 60*60*24; // in 24 hours
+config.no_retry_services = ["tt_repeat_push", "tt_repeat_alert_push", "livestats"];
 module.exports = config;
